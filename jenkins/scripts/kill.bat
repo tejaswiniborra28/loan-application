4b30c1fe206cbc0f0251fcbbe53afb2d09de4d43
Tejaswini Borra <tejaswiniborra28@gmail.com>
2022-10-31 13:48:22 +0530

    kill

--- jenkins/scripts/kill.bat
+++ jenkins/scripts/kill.bat
@@ -3,5 +3,6 @@
 echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
+echo %(cat .pidfile)%
 set -x
 taskkill /PID %(cat .pidfile)% /F
