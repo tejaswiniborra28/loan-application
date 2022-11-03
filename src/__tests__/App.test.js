6ad63429d6f1a626fecabbc761eb877197947342
unknown <tejaswiniborra28@gmail.com>
2022-11-03 13:05:47 +0530

    modified test case

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-//App test case
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import { render } from '@testing-library/react';
+import App from "../App";
+
+test('renders  App component', () => {
+    render(<App />);
+  });
